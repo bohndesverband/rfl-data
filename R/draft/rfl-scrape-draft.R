01143d3f02c4329b969692eb6b8ef21322e503c9
Jakob Eschler <mail@jakob-eschler.de>
2024-06-01 04:11:56 +0200

    code update draft data

--- R/draft/rfl-scrape-draft.R
+++ R/draft/rfl-scrape-draft.R
@@ -45,5 +45,8 @@ readr::write_csv(draft_data, paste0("rfl_draft_", var_draft_season, ".csv"))
 cli::cli_alert_info("Upload Data")
 piggyback::pb_upload(paste0("rfl_draft_", var_draft_season, ".csv"), "bohndesverband/rfl-data", "draft_data", overwrite = TRUE)
 
-write(jsonlite::toJSON(c("last_updated", format(Sys.time(), "%Y-%m-%d %X GMT"))), "timestamp.json")
+timestamp <- list(last_updated = format(Sys.time(), "%Y-%m-%d %X GMT")) |>
+  jsonlite::toJSON(auto_unbox = TRUE)
+
+write(timestamp, "timestamp.json")
 piggyback::pb_upload("timestamp.json", "bohndesverband/rfl-data", "draft_data", overwrite = TRUE)
