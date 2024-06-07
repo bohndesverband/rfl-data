4e0c5a7c9afbea12216f568ae063020973e708d8
Jakob Eschler <mail@jakob-eschler.de>
2024-06-07 19:07:35 +0200

    workflow for draft gsis ids added

---
+++ R/draft/rfl-add-gsis-to-draft.R
@@ -0,0 +1,44 @@
+library(nflreadr)
+library(tidyverse)
+
+current_season <- nflreadr::get_current_season()
+
+rfl_drafts <- purrr::map_df(2017:current_season, function(x) {
+  readr::read_csv(
+    glue::glue("https://github.com/bohndesverband/rfl-data/releases/download/draft_data/rfl_draft_{x}.csv"),
+    col_types = "iicccccccci"
+  )
+})
+
+merge <- rfl_drafts %>%
+  dplyr::filter(is.na(gsis_id)) %>%
+  dplyr::select(-gsis_id) %>%
+  dplyr::left_join(
+    nflreadr::load_ff_playerids() %>%
+      dplyr::select(mfl_id, gsis_id),
+    by = "mfl_id"
+  ) %>%
+  dplyr::select(season:mfl_id, gsis_id, player_name:is_rookie)
+
+new_data <- rbind(rfl_drafts %>%
+                    dplyr::filter(!is.na(gsis_id)),
+                  merge) %>%
+  dplyr::arrange(season, overall)
+
+for (loop_season in 2017:current_season) {
+  cli::cli_alert_info("Write Data")
+  readr::write_csv(
+    new_data %>%
+      dplyr::filter(season == loop_season),
+    glue::glue("data/drafts/rfl_draft_{season}.csv")
+  )
+
+  cli::cli_alert_info("Upload Data")
+  piggyback::pb_upload(paste0("rfl_draft_", current_season, ".csv"), "bohndesverband/rfl-data", "draft_data", overwrite = TRUE)
+}
+
+timestamp <- list(last_updated = format(Sys.time(), "%Y-%m-%d %X", tz = "Europe/Berlin")) %>%
+  jsonlite::toJSON(auto_unbox = TRUE)
+
+write(timestamp, "timestamp.json")
+piggyback::pb_upload("timestamp.json", "bohndesverband/rfl-data", "draft_data", overwrite = TRUE)
