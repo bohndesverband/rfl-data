217b447fbc0c14d6173d9713c0d682c3dfb42a8b
Jakob Eschler <mail@jakob-eschler.de>
2024-09-21 02:48:19 +0200

    use ffscsrapr for scrape-draft script

--- R/draft/rfl-scrape-draft.R
+++ R/draft/rfl-scrape-draft.R
@@ -3,47 +3,39 @@ library(piggyback)
 library(nflreadr)
 
 cli::cli_alert_info("Create Data")
+draft_season <- lubridate::year(lubridate::now())
+mfl <- ffscrapr::mfl_connect(draft_season, 63018)
 
-var_draft_season <- nflreadr::get_current_season(TRUE)
-
-draft_data <- jsonlite::read_json(paste0("https://www45.myfantasyleague.com/", var_draft_season, "/export?TYPE=draftResults&L=63018&APIKEY=&JSON=1"))$draftResults$draftUnit$draftPick %>%
-  dplyr::tibble() %>%
-  tidyr::unnest_wider(1) %>%
-  dplyr::mutate(season = var_draft_season) %>%
-  dplyr::rename(mfl_id = player) %>%
-  dplyr::arrange(round, pick) %>%
-  dplyr::mutate(overall = dplyr::row_number()) %>%
+draft_data <- ffscrapr::ff_draft(mfl) %>%
+  dplyr::rename(mfl_id = player_id) %>%
   dplyr::left_join(
     nflreadr::load_ff_playerids() %>%
       dplyr::select(mfl_id, gsis_id),
     by = "mfl_id"
   ) %>%
   dplyr::left_join(
-    nflreadr::load_players() %>%
-      dplyr::select(gsis_id, display_name),
-    by = "gsis_id",
-    na_matches = "never"
-  ) %>%
-  dplyr::left_join(
-    jsonlite::read_json(paste0("https://www45.myfantasyleague.com/", var_draft_season, "/export?TYPE=players&L=63018&APIKEY=&DETAILS=&SINCE=&PLAYERS=&JSON=1"))$players$player %>%
+    jsonlite::read_json(paste0("https://www45.myfantasyleague.com/", draft_season, "/export?TYPE=players&L=63018&APIKEY=&DETAILS=&SINCE=&PLAYERS=&JSON=1"))$players$player %>%
       dplyr::tibble() %>%
-      tidyr::unnest_wider(1),
+      tidyr::hoist(1, "id", "status") %>%
+      dplyr::select(id, status),
     by = c("mfl_id" = "id")
   ) %>%
   dplyr::mutate(
-    player_name = ifelse(is.na(display_name), name, display_name),
+    season = draft_season,
+    player_name = nflreadr::clean_player_names(player_name),
     is_rookie = dplyr::case_when(
       status == "R" ~ 1,
       TRUE ~ 0
-    )
+    ),
+    team = nflreadr::clean_team_abbrs(team)
   ) %>%
-  dplyr::select(season, overall, round, pick, franchise, mfl_id, gsis_id, player_name, position, team, is_rookie)
+  dplyr::select(season, timestamp, overall, round, pick, franchise_id, franchise_name, mfl_id, gsis_id, player_name, pos, team, is_rookie)
 
 cli::cli_alert_info("Write Data")
-readr::write_csv(draft_data, paste0("rfl_draft_", var_draft_season, ".csv"))
+readr::write_csv(draft_data, paste0("rfl_draft_", draft_season, ".csv"))
 
 cli::cli_alert_info("Upload Data")
-piggyback::pb_upload(paste0("rfl_draft_", var_draft_season, ".csv"), "bohndesverband/rfl-data", "draft_data", overwrite = TRUE)
+piggyback::pb_upload(paste0("rfl_draft_", draft_season, ".csv"), "bohndesverband/rfl-data", "draft_data", overwrite = TRUE)
 
 timestamp <- list(last_updated = format(Sys.time(), "%Y-%m-%d %X", tz = "Europe/Berlin")) %>%
   jsonlite::toJSON(auto_unbox = TRUE)
