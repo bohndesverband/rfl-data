af5d030781547d76d69ca8edac69af71f82b90c1
Jakob Eschler <mail@jakob-eschler.de>
2025-02-23 22:21:26 +0100

    added player awards code

---
+++ R/awards/rfl-player-awards-data.R
@@ -0,0 +1,225 @@
+library(tidyverse)
+library(nflfastR)
+library(nflreadr)
+library(piggyback)
+
+cli::cli_alert_info("Create Data")
+
+current_season = nflreadr::get_current_season()
+current_week <- nflreadr::get_current_week()
+
+if(current_week == 14) {
+  # base data ----
+  ## fantasy ----
+  fantasyPoints <- readr::read_csv(paste0("https://github.com/bohndesverband/rfl-data/releases/download/playerscores_data/rfl_playerscores_", current_season, ".csv"), col_types = "ddccccd") %>%
+    dplyr::mutate(
+      points = as.numeric(points),
+      week = as.integer(week)
+    )
+
+
+  ## WAR ----
+  franchiseWARPlayerRaw <- readr::read_csv(paste0("https://github.com/bohndesverband/rfl-data/releases/download/war_data/rfl_war_", current_season, ".csv"), col_types = "icccdd") %>%
+    dplyr::left_join(
+      readr::read_csv(paste0("https://raw.githubusercontent.com/jak3sch/rfl/main/data/war/rfl-war-", current_season - 1, ".csv"), col_types = "icccdd") %>%
+        dplyr::select(player_id, war) %>%
+        dplyr::rename(war_last_season = war),
+      by = "player_id"
+    )
+
+  franchiseWARPlayer <- jsonlite::read_json(paste0("https://www45.myfantasyleague.com/", current_season, "/export?TYPE=rosters&L=63018&APIKEY=&FRANCHISE=&W=&JSON=1"))$rosters$franchise %>%
+    dplyr::tibble() %>%
+    tidyr::unnest_wider(1) %>%
+    dplyr::rename(franchise_id = id) %>%
+    tidyr::unnest(player) %>%
+    tidyr::unnest_wider(player) %>%
+    dplyr::rename(mfl_id = id) %>%
+    dplyr::select(mfl_id, franchise_id) %>%
+    dplyr::left_join(franchiseWARPlayerRaw %>% dplyr::select(player_id, pos, points, war, war_last_season), by = c("mfl_id" = "player_id"), multiple = "any") %>%
+    dplyr::mutate(
+      war_per_fpt = war / points
+    ) %>%
+    group_by(franchise_id) %>%
+    arrange(desc(war_per_fpt)) %>%
+    dplyr::ungroup()
+
+  ## Official Stats ----
+  pbp <- nflreadr::load_pbp()
+  playerStats <- nflfastR::calculate_stats(current_season, "season", "player", "REG")
+
+  # awards ----
+  orderCols <- function(df) {
+    df %>%
+      utils::head(3) %>%
+      dplyr::mutate(rank = dplyr::row_number()) %>%
+      dplyr::select(mfl_id, gsis_id, pos, points, war, rank)
+  }
+
+  player_awards <- franchiseWARPlayer %>%
+    dplyr::select(mfl_id, pos, points, war, war_last_season) %>%
+    dplyr::distinct() %>%
+    dplyr::group_by(mfl_id, pos, points, war, war_last_season) %>%
+    dplyr::left_join(nflreadr::load_ff_playerids() %>% dplyr::select(mfl_id, gsis_id), by = "mfl_id") %>%
+    dplyr::filter(!is.na(war)) %>%
+    dplyr::group_by(pos) %>%
+    dplyr::arrange(desc(war)) %>%
+    dplyr::mutate(
+      rank = dplyr::row_number(),
+      # manual gsis_ids
+      #gsis_id = dplyr::case_when(
+    #    mfl_id == "15853" ~ "00-0038133",
+      #  mfl_id == "15832" ~ "00-0037237",
+       # mfl_id == "15849" ~ "00-0038125",
+      #  mfl_id == "15894" ~ "00-0037079",
+      #  mfl_id == "15833" ~ "00-0035253",
+      #  mfl_id == "15815" ~ "00-0037235",
+      #  mfl_id == "15812" ~ "00-0038127",
+      #  TRUE ~ gsis_id
+      #)
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::left_join(
+      nflreadr::load_players() %>% dplyr::select(display_name, last_name, gsis_id, rookie_year),
+      by = "gsis_id",
+      multiple = "first"
+    )
+
+  ## MVP ----
+  player_mvp <- player_awards %>%
+    dplyr::arrange(desc(war)) %>%
+    orderCols() %>%
+    dplyr::mutate(
+      award = "MVP",
+      label = ifelse(rank == 1, "Most Valuable Player", paste0(rank, ". Platz"))
+    )
+
+  ## OPOY ----
+  player_opoy <- player_awards %>%
+    dplyr::filter(pos %in% c("QB", "RB", "WR", "TE") & mfl_id != player_mvp$mfl_id[1]) %>%
+    dplyr::arrange(desc(war)) %>%
+    orderCols() %>%
+    dplyr::mutate(
+      award = "OPOY",
+      label = ifelse(rank == 1, "Offensive Player of the Year", paste0(rank, ". Platz"))
+    )
+
+  ## DPOY ----
+  player_dpoy <- player_awards %>%
+    dplyr::filter(pos %in% c("DT", "DE", "LB", "CB", "S") & mfl_id != player_mvp$mfl_id[1]) %>%
+    dplyr::arrange(desc(war)) %>%
+    orderCols() %>%
+    dplyr::mutate(
+      award = "DPOY",
+      label = ifelse(rank == 1, "Defensive Player of the Year", paste0(rank, ". Platz"))
+    )
+
+  ## AIR POY ----
+  player_air <- playerStats %>%
+    dplyr::filter(position == "QB") %>%
+    dplyr::mutate(
+      fpts_air =
+        (passing_tds * 4) +
+        (passing_yards * 0.04) +
+        (passing_2pt_conversions * 2) +
+        (passing_interceptions * -1) +
+        (sack_fumbles_lost * -1)
+    ) %>%
+    dplyr::select(player_id, fpts_air) %>%
+    dplyr::arrange(desc(fpts_air)) %>%
+    dplyr::rename(gsis_id = player_id) %>%
+    dplyr::left_join(player_awards, by = "gsis_id", multiple = "first") %>%
+    orderCols() %>%
+    dplyr::mutate(
+      award = "APOY",
+      label = ifelse(rank == 1, "Air Player of the Year", paste0(rank, ". Platz"))
+    )
+
+  ## GROUND POY ----
+  player_ground <- playerStats %>%
+    dplyr::mutate(
+      fpts_air =
+        (rushing_tds * 6) +
+        (rushing_yards * 0.1) +
+        (rushing_2pt_conversions * 2) +
+        (rushing_fumbles_lost * -1)
+    ) %>%
+    dplyr::select(player_id, fpts_air) %>%
+    dplyr::arrange(desc(fpts_air)) %>%
+    dplyr::rename(gsis_id = player_id) %>%
+    dplyr::left_join(player_awards, by = "gsis_id", multiple = "first") %>%
+    orderCols() %>%
+    dplyr::mutate(
+      award = "GPOY",
+      label = ifelse(rank == 1, "Ground Player of the Year", paste0(rank, ". Platz"))
+    )
+
+  ## Rookies ----
+  player_rookies <- player_awards %>%
+    dplyr::filter(rookie_year == current_season)
+
+  ### OROY ----
+  player_oroy <- player_rookies %>%
+    dplyr::filter(pos %in% c("QB", "RB", "WR", "TE")) %>%
+    dplyr::arrange(desc(war)) %>%
+    orderCols() %>%
+    dplyr::mutate(
+      award = "OROY",
+      label = ifelse(rank == 1, "Offensive Rookie of the Year", paste0(rank, ". Platz"))
+    )
+
+  ### DROY ----
+  player_droy <- player_rookies %>%
+    dplyr::filter(pos %in% c("DT", "DE", "LB", "CB", "S")) %>%
+    dplyr::arrange(desc(war)) %>%
+    orderCols() %>%
+    dplyr::mutate(
+      award = "DROY",
+      label = ifelse(rank == 1, "Defensive Rookie of the Year", paste0(rank, ". Platz"))
+    )
+
+  ## Comeback
+  player_comeback <- nflreadr::load_rosters(current_season - 1) %>%
+    dplyr::filter(status == "RES") %>%
+    dplyr::select(gsis_id) %>%
+    dplyr::left_join(
+      player_awards,
+      by = "gsis_id"
+    ) %>%
+    dplyr::filter(war >= 0) %>%
+    dplyr::mutate(war_diff = war - war_last_season) %>%
+    dplyr::arrange(desc(war_diff)) %>%
+    orderCols() %>%
+    dplyr::mutate(
+      award = "CPOY",
+      label = ifelse(rank == 1, "Comeback Player of the Year", paste0(rank, ". Platz"))
+    )
+
+  combined_player_awards <- rbind(player_mvp, player_opoy, player_dpoy, player_air, player_ground, player_oroy, player_droy, player_comeback) %>%
+    dplyr::left_join(
+      player_awards %>%
+        dplyr::select(mfl_id, dplyr::ends_with("name")),
+      by = "mfl_id"
+    ) %>%
+    dplyr::mutate(season = current_season) %>%
+    dplyr::select(season, dplyr::ends_with("id"), dplyr::ends_with("name"), pos:label) %>%
+    dplyr::mutate(points = round(points, 2))
+
+  read_data <- readr::read_csv("https://github.com/bohndesverband/rfl-data/releases/download/awards_data/rfl-player-awards.csv", col_types = "icccccddicc") %>%
+    dplyr::filter(season < current_season)
+
+  cli::cli_alert_info("Write Data")
+  readr::write_csv(rbind(read_data, combined_player_awards), "rfl-player-awards.csv")
+
+  cli::cli_alert_info("Upload Data")
+  piggyback::pb_upload("rfl-player-awards.csv", "bohndesverband/rfl-data", "awards_data", overwrite = TRUE)
+
+  timestamp <- list(last_updated = format(Sys.time(), "%Y-%m-%d %X", tz = "Europe/Berlin")) %>%
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  write(timestamp, "timestamp.json")
+  piggyback::pb_upload("timestamp.json", "bohndesverband/rfl-data", "postseason_data", overwrite = TRUE)
+} else if (current_week < 14) {
+  cli::cli_alert_info("Season is not over yet")
+} else {
+  cli::cli_alert_info("Already processed player awards data")
+}
---
+++ R/awards/rfl-player-awards-plots.R
@@ -0,0 +1,269 @@
+library(tidyverse)
+library(nflreadr)
+library(piggyback)
+library(cropcircles)
+
+cli::cli_alert_info("Create Data")
+
+# variables ----
+current_season = nflreadr::get_current_season()
+current_week <- nflreadr::get_current_week()
+
+color_grey_light <- "#d2dae2"
+color_grey_mid <- "#808e9b"
+color_grey_dark <- "#485460"
+color_black <- "#1e272e"
+color_red <- "#f53b57"
+color_blue <- "#3c40c6"
+color_cyan <- "#0fbcf9"
+color_petrol <- "#00d8d6"
+color_green <- "#05c46b"
+color_orange <- "#ffa801"
+color_yellow <- "#ffd32a"
+color_bg <- "white"
+color_text <- color_black
+
+colors_position <- c(
+  "QB" = "#feca57",
+  "RB" = "#1dd1a1",
+  "WR" = "#54a0ff",
+  "TE" = "#ff6b6b",
+  "PK" = "#c8d6e5",
+  "DL" = "#48dbfb",
+  "LB" = "#ff9ff3",
+  "DB" = "#00d2d3"
+)
+
+font <- "Poppins"
+
+position_order <- c("QB", "RB", "WR", "TE", "FLX", "PK", "DL", "LB", "DB", "IDP")
+positions <- c("QB", "RB", "WR", "TE", "PK", "DL", "LB", "DB")
+
+# helper ----
+player_annotaions <- function() {
+  player_data <- current_awards %>%
+    dplyr::group_by(award) %>%
+    dplyr::filter(dplyr::row_number() == 1)
+
+  player_annotations <- list(
+    # award title
+    ggplot2::geom_text(
+      data = player_data,
+      mapping = ggplot2::aes(label = award),
+      x = 3.5, y = 1.32, hjust = 0.5, color = color_orange, size = 5
+    ),
+    # player name
+    ggplot2::geom_text(
+      data = player_data,
+      mapping = ggplot2::aes(label = display_name),
+      x = 3.5, y = -2.4, hjust = 0.5, color = color_black, size = 4.5
+    ),
+    # award description
+    ggplot2::geom_text(
+      data = player_data,
+      mapping = ggplot2::aes(label = subline),
+      x = 3.5, y = -2.65, vjust = 1, hjust = 0.5, color = color_grey_mid, size = 4, lineheight = 1
+    )
+  )
+
+  return(player_annotations)
+}
+
+segments <- data.frame(
+  x1 = c(rep(0.5, 3), 1, 4),
+  x2 = c(rep(6.5, 3), 3, 6),
+  y1 = c(0.25, 0.50, 0.75, 1.35, 1.35),
+  y2 = c(0.25, 0.50, 0.75, 1.35, 1.35),
+  color = c(rep(color_bg, 3), rep(color_orange, 2))
+)
+
+plot_default <- list(
+  ggplot2::scale_color_manual(values = colors_position, guide = "none"),
+  ggplot2::theme_void(),
+  ggplot2::theme(
+    text = ggplot2::element_text(color = color_text, family = font, lineheight = 1.2),
+    plot.margin = ggplot2::margin(t = 0, r = 0, b = 30, l = 0),
+    plot.background = ggplot2::element_rect(fill = color_bg, color = color_bg),
+
+    plot.title = ggplot2::element_text(size = 24, face = "bold", lineheight = 0.8, margin = ggplot2::margin(t = -40, b = 0, l = 25)),
+    plot.title.position = "plot",
+    plot.subtitle = ggplot2::element_text(size = 16, lineheight = 1, margin = ggplot2::margin(t = 10, b = 25, l = 25)),
+
+    legend.key.size = ggplot2::unit(12, "pt"),
+    legend.text = ggplot2::element_text(size = 12),
+  )
+)
+
+plot_default_bar <- list(
+  geom_text(mapping = ggplot2::aes(label = war), vjust = 1.7, size = 10, color = color_bg),
+  #geom_text(aes(label = paste0("#", award_rank)), vjust = 5, color = color_bg, size = 6, family = "base"),
+  geom_text(aes(label = sapply(display_name, function(x) paste(strwrap(x, width = 5), collapse = "\n"))), vjust = -0.5, hjust = 0.5, size = 7, color = color_grey_light, lineheight = 0.3),
+  geom_hline(yintercept = 0, color = color_grey_light, linewidth = 0.3),
+  #nflplotR::geom_nfl_headshots(aes(player_gsis = gsis_id), y = 0, height = 0.25, vjust = 0),
+  ggplot2::scale_fill_manual(values = colors_position, guide = "none")
+)
+
+if(current_week == 14) {
+  for (current_season in 2016:2024) {
+  # data ----
+  ## base data ----
+  rfl_awards <- readr::read_csv("https://raw.githubusercontent.com/bohndesverband/rfl-data/refs/heads/main/data/awards/rfl-player-awards.csv", col_types = "icccccddicc") %>%
+    # create pctl in different categories
+    # all award candidates
+    dplyr::mutate(
+      points_total_pctl = scales::rescale(points, c(0, 1)),
+      war_total_pctl = scales::rescale(war, c(0, 1))
+    ) %>%
+
+    # all award candidates in same year
+    dplyr::group_by(season) %>%
+    dplyr::mutate(
+      points_season_pctl = scales::rescale(points, c(0, 1)),
+      war_season_pctl = scales::rescale(war, c(0, 1))
+    ) %>%
+
+    # all award candidates in same award
+    dplyr::group_by(award) %>%
+    dplyr::mutate(
+      points_award_pctl = scales::rescale(points, c(0, 1)),
+      war_award_pctl = scales::rescale(war, c(0, 1))
+    ) %>%
+
+    dplyr::ungroup() %>%
+    tidyr::gather(category, pctl, dplyr::ends_with("pctl")) %>%
+    dplyr::mutate(
+      award_f = factor(award, c("MVP", "OPOY", "DPOY", "CPOY", "APOY", "GPOY", "OROY", "DROY")),
+      category_f= factor(
+        category,
+        levels = c("points_total_pctl", "points_season_pctl", "points_award_pctl", "war_award_pctl", "war_season_pctl", "war_total_pctl")
+      ),
+      group = dplyr::case_when(
+        grepl("total_pctl", category) ~ paste0("Alle Kandidaten 2016-", current_season),
+        grepl("award_pctl", category) ~ paste0("Alle Kandidaten im selben Award 2016-", current_season),
+        grepl("season_pctl", category) ~ paste("Alle Kandidaten", current_season),
+      ),
+      subline = dplyr::case_when(
+        award == "MVP" ~ "Meiste Wins over Replacement (WAR)",
+        award == "OPOY" ~ "Meiste WAR: Offense non-MVP",
+        award == "DPOY" ~ "Meiste WAR: Defense non-MVP",
+        award == "CPOY" ~ "Hat die letzte Saison auf IR beendet\nund seit dem die höchste WAR Differenz",
+        award == "APOY" ~ "Meiste Fantasy Punkte durch die Luft (QB)",
+        award == "GPOY" ~ "Meiste Fantasy Punkte am Boden",
+        award == "OROY" ~ "Meiste WAR: Offensive Rookie",
+        award == "DROY" ~ "Meiste WAR: Defensive Rookie"
+      )
+    ) %>%
+
+    # add images
+    dplyr::left_join(
+      nflreadr::load_players() %>%
+        dplyr::select(gsis_id, headshot),
+      by = "gsis_id"
+    )
+
+  cli::cli_alert_info("Create Current Awards")
+
+  ## current awards ----
+  current_awards <- rfl_awards %>%
+    dplyr::filter(season == current_season & rank == 1)
+
+  # plots ----
+  ## player awards ----
+  player_awards_plot <- ggplot2::ggplot(current_awards, ggplot2::aes(x = category_f, y = pctl)) +
+    ggplot2::facet_wrap(~ award_f, ncol = 4) +
+    ggplot2::coord_radial(start = -0.55 * pi, end = 0.55 * pi, clip = "off", inner.radius = 0.4) +
+
+    ggplot2::geom_col(mapping = ggplot2::aes(alpha = group), fill = color_grey_mid, width = 0.965, position = "dodge") +
+
+    # custom grid lines and annotations
+    ggplot2::geom_text(label = "FPts", x = 0.9, y = 1.55, color = color_grey_mid, size = 3, vjust = 1, hjust = 0, lineheight = 0.35) +
+    ggplot2::geom_text(label = "WAR", x = 0.45, y = -3.7, color = color_grey_mid, size = 3, vjust = 1, hjust = 1, lineheight = 0.35) +
+    ggplot2::geom_segment(data = segments, mapping = ggplot2::aes(x = x1, xend = x2, y = y1, yend = y2, color = color), linewidth = 0.5) +
+    ggplot2::scale_color_identity()
+
+  for (player in unique(rfl_awards$award)) {
+    player_awards_plot <- player_awards_plot +
+      player_annotaions()
+  }
+
+  player_awards_plot_new <- player_awards_plot +
+      ggnewscale::new_scale_color() +
+      # player background
+      ggplot2::geom_point(data = subset(current_awards, category == "points_total_pctl"), mapping = ggplot2::aes(color = pos), y = -1.06, size = 52.2) +
+
+      # player
+      ggimage::geom_image(
+        data = current_awards %>% dplyr::group_by(award) %>% dplyr::filter(dplyr::row_number() == 1),
+        mapping = ggplot2::aes(image = cropcircles::circle_crop(headshot)),
+        y = -1,
+        x = -2.2,
+        asp = 1, size = 0.56
+      ) +
+
+      ggplot2::scale_alpha_manual(values = c(0.3, 0.6, 1)) +
+
+      ggplot2::labs(
+        title = paste("RFL Spieler Auszeichnungen", current_season),
+        subtitle = "Darstellung der besten Spieler in ihren Kategorien.\nDie Balken stehen für die Perzentile, in denen die Spieler innerhalb der Top-3 Spieler jeder Kategorie (Kandidaten) ranken.",
+        alpha = "",
+        color = NULL
+      ) +
+
+      plot_default +
+      ggplot2::theme(
+        legend.position = "top",
+        strip.text = ggplot2::element_blank(),
+        panel.border = ggplot2::element_blank(),
+        panel.spacing.x = ggplot2::unit(0, "mm"),
+        panel.spacing.y = ggplot2::unit(25, "mm"),
+      )
+
+  cli::cli_alert_info("Write Current Awards")
+  ggplot2::ggsave(paste0("rfl-player-awards-plot-", current_season, ".jpg"), player_awards_plot_new, width = 2700, height = 1400, dpi = 144, units = "px")
+
+  cli::cli_alert_info("Upload Current Awards")
+  piggyback::pb_upload(paste0("rfl-player-awards-plot-", current_season, ".jpg"), "bohndesverband/rfl-data", "awards_data", overwrite = TRUE)
+}
+
+  ## all MVPs ----
+  league_mvps <- rfl_awards %>%
+    dplyr::filter(award == "MVP" & rank == 1) %>%
+    dplyr::select(season, gsis_id, display_name, pos, war) %>%
+    dplyr::distinct()
+
+  league_mvps_plot <-
+
+  ggplot2::ggplot(league_mvps, aes(x = season, y = war, fill = pos)) +
+    ggplot2::geom_col() +
+
+    plot_default_bar +
+
+    ggplot2::scale_x_continuous(breaks = c(2016:2022)) +
+    ggplot2::scale_y_continuous(limits = c(0, 6)) +
+
+    ggplot2::labs(
+      title = paste0("RFL MVPs 2016-", current_season),
+      subtitle = "Die Spieler mit den meisten Wins Above Replacement einer jeden Saison.",
+      fill = ""
+    ) +
+
+    plot_default +
+    ggplot2::theme(
+      legend.position = "top",
+      legend.margin = ggplot2::margin(t = 3, unit = "mm"),
+      axis.text.x = ggplot2::element_text(color = color_grey_light, size = 20)
+    )
+
+  cli::cli_alert_info("Write League MVPs")
+  ggplot2::ggsave(paste0("rfl-mvps.jpg"), league_mvps_plot, width = 2700, height = 1400, dpi = 144, units = "px")
+
+  timestamp <- list(last_updated = format(Sys.time(), "%Y-%m-%d %X", tz = "Europe/Berlin")) %>%
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  write(timestamp, "timestamp.json")
+  piggyback::pb_upload("timestamp.json", "bohndesverband/rfl-data", "awards_data", overwrite = TRUE)
+} else if (current_week < 14) {
+  cli::cli_alert_info("Season is not over yet")
+} else {
+  cli::cli_alert_info("Already processed player awards data")
+}
