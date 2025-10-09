1fdd39972e7c62876304213c5fae0db089dcb639
Jakob Eschler <mail@jakob-eschler.de>
2025-10-09 17:46:36 +0200

    transactions script update

--- R/transactions/rfl-scrape-transactions.R
+++ R/transactions/rfl-scrape-transactions.R
@@ -1,19 +1,28 @@
 library(nflreadr)
 library(tidyverse)
-library(ffscrapr)
 
 cli::cli_alert_info("Create Data")
 current_season <- nflreadr::get_current_season(TRUE)
 
-mfl <- ffscrapr::mfl_connect(current_season, 63018)
-
-transactions <- ffscrapr::ff_transactions(mfl, "WAIVER,TRADE,FREE_AGENT") %>%
-  dplyr::select(-comments) %>%
+transactions <- jsonlite::read_json(paste0("https://www45.myfantasyleague.com/", current_season, "/export?TYPE=transactions&L=63018&TRANS_TYPE=WAIVER,FREE_AGENT&JSON=1"))$transactions$transaction %>%
+  dplyr::tibble() %>%
+  tidyr::unnest_wider(1) %>%
+  dplyr::rowwise() %>%
   dplyr::mutate(
+    # split transaction on | and get first element
+    added = ifelse(type == "FREE_AGENT", stringr::str_split(transaction, "\\|")[[1]][1], added),
+    dropped = ifelse(type == "FREE_AGENT", stringr::str_split(transaction, "\\|")[[1]][2], added),
     season = current_season,
-    player_name = nflreadr::clean_player_names(player_name)
+    #timestamp = lubridate::as_datetime(as.numeric(timestamp), tz = "GMT"),
   ) %>%
-  dplyr::select(season, everything())
+  tidyr::gather(type_desc, player_ids, c(added, dropped)) %>%
+  dplyr::select(season, timestamp, type, type_desc, franchise, player_ids) %>%
+  # filter empty player_ids
+  dplyr::filter(player_ids != "") %>%
+  # split player_ids on , and unnest longer
+  tidyr::separate_rows(player_ids, sep = ",") %>%
+  dplyr::filter(player_ids != "") %>%
+  dplyr::rename(franchise_id = franchise, player_id = player_ids)
 
 cli::cli_alert_info("Write Data")
 readr::write_csv(transactions, paste0("rfl_transactions_", current_season, ".csv"))
